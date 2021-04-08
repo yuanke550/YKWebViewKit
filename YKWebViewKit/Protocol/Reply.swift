3d2d99ebbd1da5d11e2f01c1ca09b1fa53ade449
yuanke <yuanke550@qq.com>
2021-04-08 10:04:47 +0800

    修改getReplyString方法为协议默认实现

--- YKWebViewKit/Protocol/Reply.swift
+++ YKWebViewKit/Protocol/Reply.swift
@@ -15,11 +15,17 @@ public protocol ReplyScript: class, Script {
 public protocol ReplyScriptAble: ReplyScript {
 	/// 参数编码
 	func encodeParm() -> String
-	/// 脚本拼接
-	func getReplyString() -> String
+	
 }
 
 extension ReplyScriptAble {
+	
+	/// 脚本拼接
+	/// - Returns: 用于交互的脚本字符串
+	func getReplyString() -> String {
+		return "\(functionName)('\(encodeParm())')"
+	}
+	
 	/// 回复web
 	/// - Returns: 异步返回
 	public func reply() -> Promise<Void> {
